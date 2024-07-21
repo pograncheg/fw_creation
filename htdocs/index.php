dd0716717263faef7aa8bca8ecd27466ab753584
Alex Pogran <pograncheg@gmail.com>
2024-07-21 18:36:50 +0300

    Исправление кода в этапе №1

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,22 +2,6 @@
 
 require_once './Fw/init.php';
 
-$url = $_SERVER['REQUEST_URI'];
-echo $url . '<br>';
+echo 'Это точка входа в приложение';
 
-preg_match('#^/news/#', $url, $matches);
-print_r($matches);
-
-// $a = new Fw\Core\Config;
-// $b = Fw\Core\Application::getInstance();
-// $c = Fw\Core\Application::getInstance();
-// $route = new \Fw\Core\Route;
-// $path = include_once('./Fw/config.php');
-// print_r($path);
-// $route->doRoute($url);
-
-// echo ($a->get('db/login'));
-// include_once ('./handlers/test1.php');
-// print_r($config);
-// echo __DIR__;
 
