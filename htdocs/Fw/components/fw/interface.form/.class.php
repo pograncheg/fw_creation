aa233e1ee27d10b9799c662e69ecf9f951bfc078
Alex Pogran <pograncheg@gmail.com>
2024-08-06 15:31:29 +0300

    Вывод названия компонента

--- htdocs/Fw/components/fw/interface.form/.class.php
+++ htdocs/Fw/components/fw/interface.form/.class.php
@@ -9,7 +9,7 @@ class Form extends \Fw\Core\Component\Base
         $this->result['title'] = ['Название страницы'];
         $this->result['h2'] = ['наш шаблон'];
         $this->template->includeTemplate();
-        \Fw\Core\InstanceContainer::getInstance(\Fw\Core\Page::class)->setProperty($this->id . '_title', 'Форма');
+        \Fw\Core\InstanceContainer::getInstance(\Fw\Core\Page::class)->setProperty($this->id . '_title', 'Здесь будет наш компонент');
     }
 
 }
